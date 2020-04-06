00f8fc454d4e5fd16aef870a6971f96a1e2f5721
Igor Ždrnja <igor.z@bitcoin.com>
2020-04-06 14:26:43 +0200

    Initial commit

---
+++ src/components/FinalScore.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class FinalScore extends React.Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default FinalScore
---
+++ src/components/SelectProfileType.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/global.css'
+
+class SelectProfileType extends React.Component {
+    startQuiz(type) {
+
+    }
+
+    render() {
+
+    }
+}
+
+export default SelectProfileType
