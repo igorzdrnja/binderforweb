50fac5dc52b741f68ac733da4b09301be1f5fdec
Igor Ždrnja <igor.z@bitcoin.com>
2020-04-09 02:15:29 +0200

    Updates

---
+++ precache-manifest.7444f461fc5fcaaf9ab65fc0699e0a88.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83119b43b67a38f4a0ee1c1fc5cc04da",
+    "url": "/binderforweb/index.html"
+  },
+  {
+    "revision": "b0f5d70173242188dbe8",
+    "url": "/binderforweb/static/css/main.02d17d6c.chunk.css"
+  },
+  {
+    "revision": "bb8f210f11860f1cdcd6",
+    "url": "/binderforweb/static/js/2.e9a43bec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/binderforweb/static/js/2.e9a43bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f5d70173242188dbe8",
+    "url": "/binderforweb/static/js/main.4f836baa.chunk.js"
+  },
+  {
+    "revision": "5259991e356b4db3d962",
+    "url": "/binderforweb/static/js/runtime-main.35dc8307.js"
+  },
+  {
+    "revision": "6021dfe8dec0b7e6578d6acb473c0121",
+    "url": "/binderforweb/static/media/AirbnbCerealBold.6021dfe8.woff"
+  },
+  {
+    "revision": "886339361d3ff50e8a95ac5cdc63a82e",
+    "url": "/binderforweb/static/media/AirbnbCerealBook.88633936.woff"
+  },
+  {
+    "revision": "220b1dd31cbf1b11825433158d644def",
+    "url": "/binderforweb/static/media/AirbnbCerealMedium.220b1dd3.woff"
+  },
+  {
+    "revision": "07e90ae46404cfbddefe955b56c9cfcf",
+    "url": "/binderforweb/static/media/arrow-left.07e90ae4.png"
+  },
+  {
+    "revision": "62f0bfd2041612e9f03868e994dfd56e",
+    "url": "/binderforweb/static/media/arrow-right.62f0bfd2.png"
+  },
+  {
+    "revision": "d4355ef0f79b1258ad1f62873190d297",
+    "url": "/binderforweb/static/media/blacktown-logo.d4355ef0.png"
+  },
+  {
+    "revision": "9353c0ae766784b7c79a4ec8352744ae",
+    "url": "/binderforweb/static/media/community-profile.9353c0ae.png"
+  },
+  {
+    "revision": "122d56055b5ea17fc4050bf2b7b99410",
+    "url": "/binderforweb/static/media/info-icon.122d5605.png"
+  },
+  {
+    "revision": "7b8a5e32bfe1628cb2f3d17f620621ad",
+    "url": "/binderforweb/static/media/other-bin-closed.7b8a5e32.png"
+  },
+  {
+    "revision": "189378c170568c44cca336b016a072da",
+    "url": "/binderforweb/static/media/other-bin-opened.189378c1.png"
+  },
+  {
+    "revision": "f1cfb08769bdc8088dabef657b5d3551",
+    "url": "/binderforweb/static/media/other-bin.f1cfb087.png"
+  },
+  {
+    "revision": "371922895d1391f386f2cfe22ec629e1",
+    "url": "/binderforweb/static/media/question-img.37192289.png"
+  },
+  {
+    "revision": "09093980095091023e3c883f352fe8bc",
+    "url": "/binderforweb/static/media/red-bin-closed.09093980.png"
+  },
+  {
+    "revision": "32a61159f5117b4ef817215e8c2517d2",
+    "url": "/binderforweb/static/media/red-bin-opened.32a61159.png"
+  },
+  {
+    "revision": "fa5f0e11b5bfcf6aaa6d6b81cbad6770",
+    "url": "/binderforweb/static/media/red-bin.fa5f0e11.png"
+  },
+  {
+    "revision": "7d96e428cd90d1deb00b2f102d1907c3",
+    "url": "/binderforweb/static/media/student-profile.7d96e428.png"
+  },
+  {
+    "revision": "4ba29b456717c2b0e1ad0e5958f21299",
+    "url": "/binderforweb/static/media/yellow-bin-closed.4ba29b45.png"
+  },
+  {
+    "revision": "18619ee0009cc5d3bb98d64aa858adbc",
+    "url": "/binderforweb/static/media/yellow-bin-opened.18619ee0.png"
+  },
+  {
+    "revision": "19fac20958bf1f735593a7f4d6a16372",
+    "url": "/binderforweb/static/media/yellow-bin.19fac209.png"
+  }
+]);
