f424c17eea56a69722f953906a5a3e9b984d9cc3
Vladimir Novakovic <vladimir.novakovic@computerrock.com>
2020-05-21 23:39:09 +0200

    Adding drag and drop preview

--- src/components/QuestionsAndAnswers.js
+++ src/components/QuestionsAndAnswers.js
@@ -3,8 +3,9 @@ import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {submitQuestionAnswer, getNextQuestion, finishQuiz, resetApp} from "../store/actions";
 import { DndProvider } from 'react-dnd'
-import TouchBackend from 'react-dnd-touch-backend'
-// import htmlBackend from 'react-dnd-html5-backend'
+import Preview from 'react-dnd-preview';
+import backend from 'react-dnd-touch-backend'
+// import backend from 'react-dnd-html5-backend'
 import ConfirmBackOutOfQuiz from './ConfirmBackOutOfQuiz';
 import DraggableQuestionWrapper from './DraggableQuestionWrapper';
 import DropableAnswerWrapper from './DropableAnswerWrapper';
@@ -73,6 +74,28 @@ class QuestionsAndAnswers extends React.Component {
         this.props.getNextQuestion();
     };
 
+    previewGenerator = ({itemType, item, style}) => {
+        const {question} = this.props;
+        const {answer} = this.state;
+        const questionImage = question.QuestionImagePath ? question.QuestionImagePath : questionImagePlaceholder;
+        const customStyle = {
+            ...style,
+            opacity: 0.75
+        };
+
+        return (
+            <div style={customStyle}>
+                <div style={{transform: 'scale(0.5)'}}>
+                    <QuestionCard
+                        isAnswered={!!answer}
+                        questionImage={questionImage}
+                        questionText={question.QuestionText}
+                    />
+                </div>
+            </div>
+        );
+    };
+
     render() {
         const {question, questionIndex, numberOfQuestions} = this.props;
         const {answer} = this.state;
@@ -95,107 +118,108 @@ class QuestionsAndAnswers extends React.Component {
         const questionImage = question.QuestionImagePath ? question.QuestionImagePath : questionImagePlaceholder;
 
         return (
-            <DndProvider backend={TouchBackend} options={dndOptions}>
-            <div className="content-wrapper">
-                <div className="quiz-card-container">
-                    <div className="quiz-card-header">
-                        <div className="back-btn-wrapper">
-                            <ButtonWrapper
-                                onClick={this.showConfirmationDialog}
-                                className="btn blue-button"
-                                data-toggle="modal"
-                                data-target="#backButtonModal"
-                            >
-                                Back
-                            </ButtonWrapper>
+            <DndProvider backend={backend} options={dndOptions}>
+                <div className="content-wrapper">
+                    <div className="quiz-card-container">
+                        <div className="quiz-card-header">
+                            <div className="back-btn-wrapper">
+                                <ButtonWrapper
+                                    onClick={this.showConfirmationDialog}
+                                    className="btn blue-button"
+                                    data-toggle="modal"
+                                    data-target="#backButtonModal"
+                                >
+                                    Back
+                                </ButtonWrapper>
+                            </div>
+                            <div className="progress-bar-wrapper">
+                                <div className="progress-bar" style={progressStyle}>{questionIndex + 1} of {numberOfQuestions}</div>
+                            </div>
+                            <div className="logo-small-wrapper" />
                         </div>
-                        <div className="progress-bar-wrapper">
-                            <div className="progress-bar" style={progressStyle}>{questionIndex + 1} of {numberOfQuestions}</div>
-                        </div>
-                        <div className="logo-small-wrapper" />
-                    </div>
-                    <div className={cardHolderClassName}>
-                        {!answer ? (
-                            <DraggableQuestionWrapper>
-                                <QuestionCard
-                                    isAnswered={!!answer}
-                                    questionImage={questionImage}
-                                    questionText={question.QuestionText}
-                                />
-                            </DraggableQuestionWrapper>
-                        ) : null }
+                        <div className={cardHolderClassName}>
+                            {!answer ? (
+                                <DraggableQuestionWrapper>
+                                    <QuestionCard
+                                        isAnswered={!!answer}
+                                        questionImage={questionImage}
+                                        questionText={question.QuestionText}
+                                    />
+                                </DraggableQuestionWrapper>
+                            ) : null }
 
-                        {answer ? (
-                            <AnswerCard
-                                questionImage={questionImage}
-                                answerResponseText={answer.AnswerResponseText}
-                                isCorrectAnswer={answer.IsCorrectAnswer}
-                            />
-                        ) : null }
-                    </div>
-                    <div className="bins-wrapper">
-                        {answers.red ? (
-                            <DropableAnswerWrapper
-                                className={redClassName}
-                                onDrop={() => { this.handleSelectAnswer(answers.red, 'red');}}
-                                onClick={() => { this.handleSelectAnswer(answers.red, 'red');}}
-                            >
-                                <div
-                                    id="redBin"
+                            {answer ? (
+                                <AnswerCard
+                                    questionImage={questionImage}
+                                    answerResponseText={answer.AnswerResponseText}
+                                    isCorrectAnswer={answer.IsCorrectAnswer}
+                                />
+                            ) : null }
+                        </div>
+                        <div className="bins-wrapper">
+                            {answers.red ? (
+                                <DropableAnswerWrapper
+                                    className={redClassName}
+                                    onDrop={() => { this.handleSelectAnswer(answers.red, 'red');}}
                                     onClick={() => { this.handleSelectAnswer(answers.red, 'red');}}
                                 >
-                                    <div className="bin-name">General waste</div>
-                                </div>
-                            </DropableAnswerWrapper>
-                        ) : null}
-                        {answers.green ? (
-                            <DropableAnswerWrapper
-                                className={otherClassName}
-                                onDrop={() => { this.handleSelectAnswer(answers.green, 'other');}}
-                                onClick={() => { this.handleSelectAnswer(answers.green, 'other');}}
-                            >
-                                <div
-                                    id="otherBin"
+                                    <div
+                                        id="redBin"
+                                        onClick={() => { this.handleSelectAnswer(answers.red, 'red');}}
+                                    >
+                                        <div className="bin-name">General waste</div>
+                                    </div>
+                                </DropableAnswerWrapper>
+                            ) : null}
+                            {answers.green ? (
+                                <DropableAnswerWrapper
+                                    className={otherClassName}
+                                    onDrop={() => { this.handleSelectAnswer(answers.green, 'other');}}
                                     onClick={() => { this.handleSelectAnswer(answers.green, 'other');}}
                                 >
-                                    <div className="bin-name">Other</div>
-                                </div>
-                            </DropableAnswerWrapper>
-                        ) : null}
-                        {answers.yellow && !answer ? (
-                            <DropableAnswerWrapper
-                                className={yellowClassName}
-                                onDrop={() => { this.handleSelectAnswer(answers.yellow, 'yellow');}}
-                                onClick={() => { this.handleSelectAnswer(answers.yellow, 'yellow');}}
-                            >
-                                <div
-                                    id="yellowBin"
+                                    <div
+                                        id="otherBin"
+                                        onClick={() => { this.handleSelectAnswer(answers.green, 'other');}}
+                                    >
+                                        <div className="bin-name">Other</div>
+                                    </div>
+                                </DropableAnswerWrapper>
+                            ) : null}
+                            {answers.yellow && !answer ? (
+                                <DropableAnswerWrapper
+                                    className={yellowClassName}
+                                    onDrop={() => { this.handleSelectAnswer(answers.yellow, 'yellow');}}
                                     onClick={() => { this.handleSelectAnswer(answers.yellow, 'yellow');}}
                                 >
-                                    <div className="bin-name">Recycling</div>
-                                </div>
-                            </DropableAnswerWrapper>
-                        ) : null}
+                                    <div
+                                        id="yellowBin"
+                                        onClick={() => { this.handleSelectAnswer(answers.yellow, 'yellow');}}
+                                    >
+                                        <div className="bin-name">Recycling</div>
+                                    </div>
+                                </DropableAnswerWrapper>
+                            ) : null}
 
-                        {answer ? (
-                            <div className="next-arrow-wrapper">
-                                <div className="next-arrow-holder">
-                                    <ButtonWrapper className="next-arrow" onClick={this.handleNextArrowClick} />
+                            {answer ? (
+                                <div className="next-arrow-wrapper">
+                                    <div className="next-arrow-holder">
+                                        <ButtonWrapper className="next-arrow" onClick={this.handleNextArrowClick} />
+                                    </div>
+                                    <div className="next-arrow-name">Next</div>
                                 </div>
-                                <div className="next-arrow-name">Next</div>
-                            </div>
-                        ) : null}
+                            ) : null}
+                        </div>
                     </div>
-                </div>
 
-                {this.state.showConfirmationDialog
-                    ? <ConfirmBackOutOfQuiz
-                        onCancelHandler={this.hideConfirmationDialog}
-                        onConfirmHandler={this.confirmConfirmationDialog}
-                    />
-                    : null
-                }
-            </div>
+                    {this.state.showConfirmationDialog
+                        ? <ConfirmBackOutOfQuiz
+                            onCancelHandler={this.hideConfirmationDialog}
+                            onConfirmHandler={this.confirmConfirmationDialog}
+                        />
+                        : null
+                    }
+                </div>
+                <Preview generator={this.previewGenerator} />
             </DndProvider>
         )
     }
